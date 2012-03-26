0d88e8cdf94432f233267d9dae48369c248b7415
borteo <borgato.matteo@gmail.com>
2012-03-27 01:02:38 +0200

    added curriculum

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,10 @@ class HomeController < ApplicationController
   def index
   end
 
+  def download_pdf 
+    send_data("#{RAILS_ROOT}/public/matteo_borgato_cv.pdf", 
+      :filename => "matteo_borgato_cv.pdf",
+      :type => "application/pdf") 
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Portfolio::Application.routes.draw do
 
+  match '/download_pdf',    :to => 'home#download_pdf'
   root :to => "home#index"
-
 
 end
