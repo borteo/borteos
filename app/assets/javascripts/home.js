8c726ae83aa857ec3a6b39a08bed89ffcaa5d650
borteo <borgato.matteo@gmail.com>
2012-04-06 00:25:50 +0200

    problem with stupid me

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $('nav').scrollspy();
+  
+  $('.navbar a,footer a.up').click(function(e){
+    $.scrollTo( this.hash || 0, 1500);
+    e.preventDefault();
+  });
+  
+  $('#about-me .picture').popover({
+    placement: "right",
+    delay: { show: 10, hide: 200 }
+  });
+  
+  $.getJSON("http://twitter.com/statuses/user_timeline/borteo.json?callback=?", function(data) {
+    console.log(data[0].text);
+  });
+  
+});
