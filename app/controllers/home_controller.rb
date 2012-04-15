1d885e0d561f55c89d9b17716516c8cd62d006f6
borteo <borgato.matteo@gmail.com>
2012-04-15 23:13:00 +0200

    changed PDF name

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,4 @@ class HomeController < ApplicationController
   def index
   end
 
-  def download_pdf 
-    send_data("#{RAILS_ROOT}/public/matteoborgato_cv.pdf", 
-      :filename => "matteoborgato_cv.pdf",
-      :type => "application/pdf") 
-  end
-
 end
